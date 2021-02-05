9f01c888ee2851aad41b8cd3c3b6576ee17b55fa
mspcity <codepark2019@gmail.com>
2021-02-04 21:47:35 -0500

    created create in controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,5 +5,12 @@ class BooksController < ApplicationController
 
   def create
     # some logic
+    book = Book.new(title: 'Harry Potter 1', author: 'JK Rowling')
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unproccessable_entity
+    end
   end
 end
