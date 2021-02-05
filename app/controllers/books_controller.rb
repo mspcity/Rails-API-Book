d1294e6ee2fa872872c696d0ebd41b44f00c8e09
mspcity <codepark2019@gmail.com>
2021-02-04 21:55:41 -0500

    added private book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,18 @@ class BooksController < ApplicationController
 
   def create
     # some logic
-    book = Book.new(title: 'Harry Potter 1', author: 'JK Rowling')
+    book = Book.new(book_params)
 
     if book.save
       render json: book, status: :created
     else
       render json: book.errors, status: :unproccessable_entity
     end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
